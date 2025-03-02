6ffdcc2858f6eb058bbf924a2aa3db192fa85df0
Egor Fortov <egorfortov@gmail.com>
2025-03-02 23:15:14 +0300

    fixed docker-compose

--- main-service/src/main/java/ru/practicum/participation/ParticipationController.java
+++ main-service/src/main/java/ru/practicum/participation/ParticipationController.java
@@ -2,7 +2,6 @@ package ru.practicum.participation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.participation.dto.ParticipationRequestDto;
 
