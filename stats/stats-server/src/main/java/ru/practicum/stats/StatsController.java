a370a664172300978db28b17c806153fead2c3dc
Egor Fortov <egorfortov@gmail.com>
2025-03-02 21:42:11 +0300

    cleared code

--- stats/stats-server/src/main/java/ru/practicum/stats/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/StatsController.java
@@ -2,8 +2,6 @@ package ru.practicum.stats;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,13 +21,13 @@ public class StatsController {
     private final HitMapper mapper;
 
     @GetMapping
-    public ResponseEntity<List<StatDto>> getStats(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+    public List<StatDto> getStats(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
                                                   @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                                   @RequestParam(required = false) List<String> uris,
                                                   @RequestParam(required = false) boolean unique) {
 
         List<Stat> stats = service.getStats(start, end, uris, unique);
 
-        return new ResponseEntity<>(mapper.toStatDto(stats), HttpStatus.OK);
+        return mapper.toStatDto(stats);
     }
 }
